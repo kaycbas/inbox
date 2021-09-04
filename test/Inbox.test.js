95e700853fe11d0f4615226e20f56e1e76cee404
Kevin Bastoul <kaycbas@gmail.com>
2021-09-03 19:12:08 -0700

    test the contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,6 +4,7 @@ const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
 
+const INITIAL_MESSAGE = 'Hi there!';
 let accounts;
 let inbox;
 
@@ -14,12 +15,25 @@ beforeEach(async () => {
     // Use one of those accounts to deploy
     // the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
         .send({ from: accounts[0], gas: '1000000' })
 })
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_MESSAGE);
+    });
+
+    it('can change the message', async () => {
+        const newMessage = 'Hey, new message!';
+        await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+
+        const message = await inbox.methods.message().call();
+        assert.equal(message, newMessage);
     });
 });
