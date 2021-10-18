df2e213c61a2e1015fc762f103d4f20f1192ca31
Kevin Bastoul <kaycbas@gmail.com>
2021-10-17 18:20:49 -0700

    create first ethereum smart contract, test suite, and deploy script

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,7 +1,6 @@
+// ignore linter warning
 pragma solidity ^0.4.17;
-// linter warnings (red underline) about pragma version can igonored!
 
-// contract code will go here
 contract Inbox {
     string public message;
     
--- deploy.js
+++ deploy.js
@@ -1,1 +1,25 @@
-// deploy code will go here
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'hood hazard another amazing liquid park anxiety action pioneer party side bind',
+    'https://rinkeby.infura.io/v3/c95be3e0f64b4ebf9c3fd60180437466'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+    
+    // result will be an instance of the contract
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+}
+
+deploy();
